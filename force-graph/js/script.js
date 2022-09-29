59be34b3b26b75a5254b80215157f74f9205c21e
Alberto Harres <alberto.harres@gmail.com>
2022-09-29 13:27:47 +0200

    fix

--- force-graph/js/script.js
+++ force-graph/js/script.js
@@ -11,8 +11,9 @@ const highlightLinks = new Set();
 var node_index = 0
 var Graph = null
 var fontFace = null
+var savedCameraPos = null
 
-fetch(`../export/narratives_word_graphs/${narrative}.json`)
+fetch(`../../export/narratives_word_graphs/${narrative}.json`)
   .then((response) => response.json())
   .then((data) => {
     console.log(data)
@@ -50,48 +51,50 @@ const init = function (gData) {
     focusNode = node || null;
     updateHighlight();
   })
-  .nodeThreeObject(nodeSpriteObject);
+  .nodeThreeObject((node) => {
+    const group = new THREE.Group();
+    const geometry = new THREE.SphereGeometry(2, 32, 64);
+    const material = new THREE.MeshBasicMaterial({ color: node.color });
+    const sphere = new THREE.Mesh(geometry, material);
+    group.add(sphere);
+  
+    const sprite = new SpriteText(node[lang]);
+    sprite.position.set(2, 10, 0);
+  
+    sprite.fontFace = "roboto-mono";
+    sprite.material.depthWrite = false; // make sprite background transparent
+    sprite.color = "#000000";
+    sprite.strokeColor = node.color;
+    // sprite.position.set(0, 100, 100);
+  
+    if (highlightNodes.size > 0) {
+      if (highlightNodes.has(node.id)) {
+        sprite.material.opacity = 0.9
+      } else {
+        sprite.material.opacity = 0
+      }
+    }
+  
+    sprite.textHeight = 7 + 3 * (node_index / gData.nodes.length);
+    //sprite.material.opacity = Math.max(0.35, (node_index / gData.nodes.length))
+    sprite.material.opacity = 0.8
+    console.log("node_index / gData.nodes.length", node_index / gData.nodes.length)
+    sprite.fontWeight = 'normal';
+  
+    group.add(sprite);
+    node_index++
+    return group;
+  });
 
   // no scroll zoom
-  Graph.controls().noZoom = true
+  // Graph.controls().noZoom = true
 
   // Spread nodes a little wider
   Graph.d3Force('charge').strength(-300);
-}
 
-const nodeSpriteObject = (node) => {
-  const group = new THREE.Group();
-  const geometry = new THREE.SphereGeometry(2, 32, 64);
-  const material = new THREE.MeshBasicMaterial({ color: node.color });
-  const sphere = new THREE.Mesh(geometry, material);
-  group.add(sphere);
+  // save initial camera position
+  savedCameraPos = myGraph.cameraPosition();
 
-  const sprite = new SpriteText(node[lang]);
-  sprite.position.set(2, 10, 0);
-
-  sprite.fontFace = "roboto-mono";
-  sprite.material.depthWrite = false; // make sprite background transparent
-  sprite.color = "#000000";
-  sprite.strokeColor = node.color;
-  // sprite.position.set(0, 100, 100);
-
-  if (highlightNodes.size > 0) {
-    if (highlightNodes.has(node.id)) {
-      sprite.material.opacity = 0.9
-    } else {
-      sprite.material.opacity = 0
-    }
-  }
-
-  sprite.textHeight = 7 + 3 * (node_index / gData.nodes.length);
-  //sprite.material.opacity = Math.max(0.35, (node_index / gData.nodes.length))
-  sprite.material.opacity = 0.8
-  console.log("node_index / gData.nodes.length", node_index / gData.nodes.length)
-  sprite.fontWeight = 'normal';
-
-  group.add(sprite);
-  node_index++
-  return group;
 }
 
 const updateHighlight = function () {
@@ -104,6 +107,92 @@ const updateHighlight = function () {
   //.nodeThreeObject(Graph.nodeThreeObject())
   //.linkDirectionalParticles(Graph.linkDirectionalParticles());
 }
+
+const functions = {
+  autoRotate: function () {
+    // camera orbit
+    let angle = 0;
+    setInterval(() => {
+      Graph.cameraPosition({
+        x: distance * Math.sin(angle),
+        z: distance * Math.cos(angle)
+      });
+      angle += Math.PI / 3000;
+    }, 10);
+  },
+  focusOnNode: function (params) {
+    let node_id = params.node_id
+    let distance = params.distance
+    var node = Graph.graphData().nodes.find(n => {
+      return n.id == node_id
+    })
+    if (!node) return
+    // Aim at node from outside it
+    const distRatio = 1 + distance / Math.hypot(node.x, node.y, node.z);
+  
+    const newPos = node.x || node.y || node.z
+      ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
+      : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+  
+    Graph.cameraPosition(
+      newPos, // new position
+      node, // lookAt ({ x, y, z })
+      3000  // ms transition duration
+    );
+  },
+  xf: function () {
+    var data = Graph.graphData()
+    if (node == null) {
+      data.nodes.map(n => {
+        //console.log()
+        n.__threeObj.material.opacity = parseInt(n.value) / 50
+        // sprite.color = "color";
+      })
+      return
+    }
+    var connections = data.links.filter(l => l.target.id == node.id || l.source.id == node.id).map(l => l)
+    var nodes = data.nodes.filter(n => {
+      return connections.some(l => (l.target.id == n.id || l.source.id == n.id))
+    })
+    data.nodes.map(n => {
+      n.__threeObj.material.opacity = 0.1
+    })
+    nodes.map(n => {
+      n.__threeObj.material.opacity = 1
+    })
+    console.log("connections", connections)
+    console.log("nodes", nodes)
+  },
+  fitToCanvas: function (data) {
+    // Graph.zoomToFit(data || 100)
+    Graph.cameraPosition(
+      newPos, // new position
+      node, // lookAt ({ x, y, z })
+      3000  // ms transition duration
+    );
+  },
+  setZoom: function (data) {
+    // 
+    Graph.cameraPosition(
+      savedCameraPos, // new position
+      {x: 0, y: 0, z: 0}, // lookAt ({ x, y, z })
+      3000  // ms transition duration
+    );
+  },
+  noZoom: function (set) {
+    Graph.controls().noZoom = set || true
+  }
+}
+
+window.addEventListener("message", (event) => {
+  //console.log("window message", event)
+  console.log("window message", event)
+  var fn = event.data.function
+  var data = event.data.data
+  if (functions[fn] !== undefined) {
+    functions[fn](data)
+  }
+}, false);
 
 
 function onWindowResize(){
